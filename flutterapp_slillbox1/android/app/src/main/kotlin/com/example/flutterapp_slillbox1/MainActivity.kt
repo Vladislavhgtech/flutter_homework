02d41532b38d2333cd7041b1062c56beb22723fd
Vladislavhgtech <vld.85@inbox.ru>
2022-01-18 16:33:41 +0300

    Homework_3

---
+++ flutterapp_slillbox1/android/app/src/main/kotlin/com/example/flutterapp_slillbox1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterapp_slillbox1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutterapp_slillbox1/lib/main.dart
@@ -0,0 +1,76 @@
+// Flutter imports:
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+// Package imports:
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SvgPicture.asset(
+                // подключение svg из папки
+                'assets/svg/flutterio-icon.svg',
+                semanticsLabel: 'SVG From asset folder',
+              ),
+              SvgPicture.network(// подключение SVG из сети
+                  'https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg'),
+              CarouselSlider(
+                options: CarouselOptions(height: 400.0),
+                items: [1, 2, 3, 4, 5].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(color: Colors.amber),
+                          child: Text(
+                            'text $i',
+                            style: TextStyle(fontSize: 16.0),
+                          ));
+                    },
+                  );
+                }).toList(),
+              )
+            ]),
+      ),
+
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
